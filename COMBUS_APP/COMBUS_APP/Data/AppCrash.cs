94212ca1156c3301dc8c390a740579ff3d2a18ba
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-19 01:08:58 +0700

    create appcrash

---
+++ COMBUS_APP/COMBUS_APP/Data/AppCrash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMBUS_APP.Data
+{
+    static class AppCrash
+    {
+        private static string _Login = string.Empty;
+        public static string Login
+        {
+            get { return _Login; }
+            set { _Login = value; }
+        }
+                 
+    }
+
+    static class Messge
+    {
+        public static string WRN_Error = "PROCESS ERROR PLEASE CALL SYSTEM ADMINISTRATOR !!!";
+        public static string WRN_Date_FromMoreTo = "เวลาช่วงหลังน้อยกว่าช่วงแรก";
+        public static string WRN_NumberOnly = "กรุณากรอกข้อมูลที่เป้นตัวเลข 0-9 เท่านั้น";
+        public static string WRN_NotData = "ไม่พบข้อมูล";
+
+        public static string INF_Save = "บันทึกเสร็จสิ้น";
+        public static string INF_Delete = "ลบข้อมูลเสร็จสิ้น";
+        public static string INF_Edit = "บันทึกข้อมูลเสร็จสิ้น";
+        public static string INF_Cancel = "ยกเลิกรายการสำเร็จ";
+    }
+}
