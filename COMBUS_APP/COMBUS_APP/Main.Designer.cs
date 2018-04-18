20c50372d263eb7c08ec6747d0eb1b1368bbe557
Luckystarii <siriwat.sa@ku.th>
2018-04-18 23:40:19 +0700

    111

--- COMBUS_APP/COMBUS_APP/Main.Designer.cs
+++ COMBUS_APP/COMBUS_APP/Main.Designer.cs
@@ -39,6 +39,7 @@
             this.BtnStore = new System.Windows.Forms.Button();
             this.BtnEmployee = new System.Windows.Forms.Button();
             this.BtnReport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -47,6 +48,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.BtnReport);
             this.panel1.Controls.Add(this.BtnEmployee);
             this.panel1.Controls.Add(this.BtnStore);
@@ -179,6 +181,23 @@
             this.BtnReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.BtnReport.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "   Report";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -210,6 +229,7 @@
         private System.Windows.Forms.Button BtnEmployee;
         private System.Windows.Forms.Button BtnStore;
         private System.Windows.Forms.Button BtnHome;
+        private System.Windows.Forms.Button button1;
     }
 }
 
