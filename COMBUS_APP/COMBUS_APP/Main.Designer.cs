b587cac5c6ce67226ee74a207141ff1e04881c24
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-18 23:29:37 +0700

    remain

--- COMBUS_APP/COMBUS_APP/Main.Designer.cs
+++ COMBUS_APP/COMBUS_APP/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMBUS_APP
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.panel1 = new System.Windows.Forms.Panel();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
--- COMBUS_APP/COMBUS_APP/Main.cs
+++ COMBUS_APP/COMBUS_APP/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace COMBUS_APP
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
