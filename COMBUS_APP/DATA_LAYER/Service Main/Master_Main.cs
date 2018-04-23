29e92e8c522a89b1a35515b2155709101e4e70cb
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-24 03:08:51 +0700

    service

--- COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
+++ COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DATA_LAYER.Service_Main
 {
-    class Master_Main
+    public class Master_Main
     {
+        private Model_ConnectionContainer _ContextEntity;
+        public Model_ConnectionContainer ContextEntity
+        {
+            get
+            {
+                if(_ContextEntity == null)
+                {
+                    _ContextEntity = new Model_ConnectionContainer();
+                }
+                return _ContextEntity;
+            }
+            set
+            {
+                _ContextEntity = value;
+            }
+        }
+
+        public List<Main_CheckLogin_Result> check_Login(string userName)
+        {
+            try
+            {
+                return this.ContextEntity.Main_CheckLogin(userName).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
