8c42119558bb816688690105d37543981b734f3c
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-28 18:07:27 +0700

    Revert "Revert "Revert "ggreturn"""
    This reverts commit 708db80cc2595dbc3e61fa9b157efd211fcef52d.

--- COMBUS_APP/DATA_LAYER/EF.Designer.cs
+++ COMBUS_APP/DATA_LAYER/EF.Designer.cs
@@ -1,4 +1,5 @@
-﻿// T4 code generation is enabled for model 'C:\Users\stp.b\Source\Repos\COMBUS_WAREHOUSE\COMBUS_APP\DATA_LAYER\Model_Connection.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\stp.b\Source\Repos\COMBUS_WAREHOUSE\COMBUS_APP\DATA_LAYER\EF.edmx'. 
+// T4 code generation is enabled for model 'C:\Users\Administrator.KKDV2-20150521U\Source\Repos\COMBUS_WAREHOUSE\COMBUS_APP\DATA_LAYER\Model_Connection.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
