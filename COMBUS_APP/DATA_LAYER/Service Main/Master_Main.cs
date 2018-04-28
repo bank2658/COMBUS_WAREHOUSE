2a176fd808eb2e9499468d39b7c6b343f80e71af
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-28 18:42:26 +0700

    EF

--- COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
+++ COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
@@ -7,6 +7,22 @@ using System.Threading.Tasks;
 namespace DATA_LAYER.Service_Main
 {
     class Master_Main
+    {
+        private EF_WAREHOUSEContainer _ContextEntity;
+        public EF_WAREHOUSEContainer ContextEntity
+        {
+            get
             {
+                if (_ContextEntity == null)
+                {
+                    _ContextEntity = new EF_WAREHOUSEContainer();
+                }
+                return _ContextEntity;
+            }
+            set
+            {
+                _ContextEntity = value;
+            }
+        }
     }
 }
