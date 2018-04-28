be7b0a90dc9c9337f665819fe8c18bf6df9bd9e3
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-28 18:25:07 +0700

    HHHHHHHHHHHHHHHHHHHH

--- COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
+++ COMBUS_APP/DATA_LAYER/Service Main/Master_Main.cs
@@ -6,30 +6,7 @@ using System.Threading.Tasks;
 
 namespace DATA_LAYER.Service_Main
 {
-    public class Master_Main
-    {
-        private EFContainer _ContextEntity;
-        public EFContainer ContextEntity
-        {
-            get
+    class Master_Main
             {
-                if(_ContextEntity == null)
-                {
-                    _ContextEntity = new EFContainer();
-                }
-                return _ContextEntity;
-            }
-            set
-            {
-                _ContextEntity = value;
-            }
-        }
-
-        public List<Main_CheckLogin_Result> check_Login(string userName)
-        {
-
-                return this.ContextEntity.Main_CheckLogin(userName).ToList();
-
-        }
     }
 }
